69ee6cf027d018c1efb4ed05f87449c63fc02c21
dc1899 <104665270+dc1899@users.noreply.github.com>
2024-03-11 21:22:19 -0300

    Finalizado el formulario de agenda (FE)

--- Ian-Salada-Barbershopp/model/db.php
+++ Ian-Salada-Barbershopp/model/db.php
@@ -5,7 +5,7 @@
         // Declare properties or class methods as static 
         // makes them accessible without the need to instantiate the class.
         public static function ConexionBD(){
-            $conn = new mysqli("localhost", "root", "root", "jesus"); // Modify the connection string with the correct credentials
+            $conn = new mysqli("localhost", "root", "", "jesus"); // Modify the connection string with the correct credentials
 
             // Solves problems with characters
             $conn->query("SET NAMES 'utf8'"); // IMPORTANT
--- Ian-Salada-Barbershopp/view/AgendaView.php
+++ Ian-Salada-Barbershopp/view/AgendaView.php
@@ -1,14 +1,18 @@
 <?php
-	session_start();
-	
-	
-	if($_SESSION["adm"] == 1){
-		$visible = "visible";
+session_start();
 
-	}else{
-		$visible = "hidden";
-	}
+$visible = "none"; // Valor por defecto
+$visiblelogin = "block"; // Valor por defecto
+
+if(isset($_SESSION["adm"]) && $_SESSION["adm"] == 1){
+    $visible = "block";
+	$visiblelogin = "none";
+}else{
+	$visible = "none";
+	$visiblelogin = "block";
+}
 ?>
+
 <!DOCTYPE HTML>
 <html>
 	<head>
@@ -19,10 +23,14 @@
 		<noscript><link rel="stylesheet" href="view/assets/css/noscript.css" /></noscript>
 		<style>
 			.btn-agenda{
-				visibility: <?php echo $visible; ?>;
+				display: <?php echo $visible; ?>;
 			}
 			.btn-logout{
-				visibility: <?php echo $visible; ?>;
+				display: <?php echo $visible; ?>;
+			}
+
+			#nav-user{
+				display: <?php echo $visiblelogin; ?>;
 			}
 	
 			</style>
@@ -63,43 +71,85 @@
 						<!-- Intro -->
 							<article id="intro">
 								<h2 class="major">Elige día y hora</h2>
-								<div class="calendar">
-									<div class="nav-calendar">
-										<div class="nav-item">
-											<button class="prev-month style-button">&laquo;</button>
+
+
+								<div class="div-calendar"> <!-- Div Principal -->
+									<div class="div-calendar-superior"> <!-- Div superior -->
+										<div><!-- Div izquierdo -->
+											<div class="calendar">
+												<div class="nav-calendar">
+													<div class="nav-item">
+														<button class="prev-month style-button btn-calendar-left">&laquo;</button>
+													</div>
+													<div class="month-year">
+														<span id="month"></span>
+														<span id="year"></span>
+													</div>
+													<div class="nav-item">
+														<button class="next-month style-button btn-calendar-right">&raquo;</button>
+													</div>
+												</div>
+												
+												<table id="calendar-table" class="style-table responsive-table">
+													<thead>
+														<tr>
+															<th>Lun</th>
+															<th>Mar</th>
+															<th>Mie</th>
+															<th>Jue</th>
+															<th>Vie</th>
+															<th>Sab</th>
+															<th>Dom</th>
+														</tr>
+													</thead>
+													<tbody></tbody>
+												</table>
+											</div>
 										</div>
-										<div class="month-year">
-											<span id="month"></span>
-											<span id="year"></span>
-										</div>
-										<div class="nav-item">
-											<button class="next-month style-button">&raquo;</button>
+										<div class="div-horas"><!-- Div derecho -->
+											<div class="div-horas-mañana">
+												<h4>Mañana</h4>
+												<input class="btn-hora" type="button" value="9:00">
+												<input class="btn-hora" type="button" value="10:00">
+												<input class="btn-hora" type="button" value="11:00">
+												<input class="btn-hora" type="button" value="12:00">
+											</div>
+											<div class="div-horas-tarde">
+												<h4>Tarde</h4>
+												<input class="btn-hora" type="button" value="14:00">
+												<input class="btn-hora" type="button" value="15:00">
+												<input class="btn-hora" type="button" value="16:00">
+												<input class="btn-hora" type="button" value="17:00">
+												<input class="btn-hora" type="button" value="18:00">
+												<input class="btn-hora" type="button" value="19:00">
+												<input class="btn-hora" type="button" value="20:00">
+												<input class="btn-hora" type="button" value="21:00">
+											</div>
 										</div>
 									</div>
-									
-									<table id="calendar-table" class="style-table">
-										<thead>
-											<tr>
-												<th>Lun</th>
-												<th>Mar</th>
-												<th>Mie</th>
-												<th>Jue</th>
-												<th>Vie</th>
-												<th>Sab</th>
-												<th>Dom</th>
-											</tr>
-										</thead>
-										<tbody></tbody>
-									</table>
+									<div> <!-- Div inferior -->
+									<form method="post">
+										<h4>Nombre</h4>
+										<input type="text">
+
+										<h4 class="txt-reservar">Teléfono</h4>
+										<input type="text">
+
+										<h4 class="txt-reservar">Nota (opcional)</h4>
+										<input type="text">
+
+										<input type="button" value="reservar" class="primary btn-reservar">
+									</form>
+										
+									</div>
 								</div>
+
 							</article>
 
 						<!-- Work -->
 							<article id="work">
-								<h2 class="major">Work</h2>
-								<span class="image main"><img src="view/images/pic02.jpg" alt="" /></span>
-								<p>Adipiscing magna sed dolor elit. Praesent eleifend dignissim arcu, at eleifend sapien imperdiet ac. Aliquam erat volutpat. Praesent urna nisi, fringila lorem et vehicula lacinia quam. Integer sollicitudin mauris nec lorem luctus ultrices.</p>
-								<p>Nullam et orci eu lorem consequat tincidunt vivamus et sagittis libero. Mauris aliquet magna magna sed nunc rhoncus pharetra. Pellentesque condimentum sem. In efficitur ligula tate urna. Maecenas laoreet massa vel lacinia pellentesque lorem ipsum dolor. Nullam et orci eu lorem consequat tincidunt. Vivamus et sagittis libero. Mauris aliquet magna magna sed nunc rhoncus amet feugiat tempus.</p>
+								<h2 class="major">Agenda</h2>
+								
 							</article>
 
 					</div>
@@ -112,9 +162,9 @@
 
 		<!-- BG -->
 		
-		<div id="marquee-container">
-            <div id="bg"></div>
-          </div>
+		<div class="marquee-container">
+			<div id="bg"></div>
+		</div>
 
 		<!-- Scripts -->
 			<script src="view/assets/js/jquery.min.js"></script>
